d536e7d9e49c43d28df9e7d6acf2b084989b709d
hung.pv <hphamviet83@gmail.com>
2020-05-10 22:48:38 +0700

    fix

--- example/callback.js
+++ example/callback.js
@@ -1,10 +1,15 @@
 import Oauth from "/dist/oauth-skymap.es5.js";
-const oauth = new Oauth({
-  response_type: "code",
-  client_id: "d8499dc0-8b63-11ea-86e9-b310e145d51d",
-  redirect_url: "http://127.0.0.1:5501/example/callback.html",
-  client_secret: "PXoN0H3SMQmJAchqDUz4lb2bYcAQ5CdUwPU05BEi"
-});
+const oauth = new Oauth(
+  {
+    response_type: "code",
+    client_id: "238e7b70-92d5-11ea-9632-c9f257edcfba",
+    redirect_url: "http://127.0.0.1:5501/example/callback.html",
+    client_secret: "NGDkBxmZUZM0GGh89zGCXqwM99IJ0igymazVmmv3"
+  },
+  {
+    providerUrl: "http://oauth2.howizbiz.com/"
+  }
+);
 oauth.callback().then(res => {
   console.log("res", res);
 });
