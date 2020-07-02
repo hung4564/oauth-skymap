7031c720e4b698e87fb1605fd513df624862ce07
hung.pv <hphamviet83@gmail.com>
2020-07-02 22:14:01 +0700

    update

--- example/callback.js
+++ example/callback.js
@@ -2,26 +2,35 @@ import Oauth from "./oauth-skymap.js";
 const oauth = new Oauth(
   {
     response_type: "code",
-    client_id: "897ea160-9dc0-11ea-915b-c7a3cabfc460",
-    redirect_url: "http://127.0.0.1:5645/example/callback.html",
+    client_id: "85485010-bc6e-11ea-b2bf-b346384034e6",
+    redirect_url: "http://127.0.0.1:5500/example/callback.html",
     code_challenge_method: "plain"
   },
   {
     providerUrl: "http://localhost:8000"
   }
 );
+let textDiv = document.getElementById("text-show");
+textDiv.innerText = "Lay token...";
 oauth.callback().then(res => {
+  textDiv.innerText = "Lay token xong.";
   console.log("res", res);
+
+  let x = document.getElementById("profile");
+  x.style.display = "block";
+  x = document.getElementById("logout");
+  x.style.display = "block";
 });
 
 document.getElementById("profile").addEventListener("click", () => {
-  oauth.auth.profile().then(res => {
-    console.log("profile", res);
+  oauth.auth.profile().then(person => {
+    document.getElementById("profile-show").innerHTML = JSON.stringify(person, null, 4);
   });
 });
 
 document.getElementById("logout").addEventListener("click", () => {
   oauth.auth.logOut().then(res => {
     console.log("logOut", res);
+    textDiv.innerText = "Dang xuat thanh cong.";
   });
 });
--- example/main.js
+++ example/main.js
@@ -2,8 +2,8 @@ import Oauth from "./oauth-skymap.js";
 const oauth = new Oauth(
   {
     response_type: "code",
-    client_id: "897ea160-9dc0-11ea-915b-c7a3cabfc460",
-    redirect_url: "http://127.0.0.1:5645/example/callback.html",
+    client_id: "85485010-bc6e-11ea-b2bf-b346384034e6",
+    redirect_url: "http://127.0.0.1:5500/example/callback.html",
     code_challenge_method: "plain"
   },
   {
